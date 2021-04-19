ae514896e37e6ace59fd4c0964cba5f4a91e6490
Gamerman2001 <Flash@heysparkbox.com>
2021-04-19 12:41:47 -0400

    feat: add response to commands - imported our Commands (line 3 - 5) - setup up a destructured list of commands (line 12) - We will ues a try catch block to listen for a command. If the command it there we will reach out to our file. If the file is not there we will send a message to the channel that they need to use a command from the list.

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import dotenv from "dotenv";
 import Discord from "discord.js";
+import play from './Commands/Play.js'
+import help from './Commands/Help.js'
+import list from './Commands/List.js'
 
 const client = new Discord.Client();
 dotenv.config();
 
 const { BOTPREFIX } = process.env;
 const { TOKEN } = process.env;
+const commandList = {play, help, list}
+
 
 client.once("ready", () => console.log("ready"));
 
@@ -19,6 +24,13 @@ client.on("message", (message) => {
    message.channel.send(`responded message in ${message.guild.name}`);
    message.channel.send(`${message.author.username} has triggered a command`);
    message.channel.send(`The command used for this message was ${command}`);
+
+   try {
+      //using computed property to call command sent in chatroom
+      commandList[command]()
+   } catch (error) {
+      return message.channel.send(`you need to use a command from the list ${error}`)
+   }
 });
 
 client.login(TOKEN);
