065ea8ecbaa26f0236be4e53b443ed8f706b501a
Gamerman2001 <Flash@heysparkbox.com>
2021-04-22 16:17:23 -0400

    refactor: Refactor index.js - combined my procress.env variables - truncated server messages - passed in argument to discord bot

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ import list from './Commands/List.js'
 const client = new Discord.Client();
 dotenv.config();
 
-const { BOTPREFIX } = process.env;
-const { TOKEN } = process.env;
+const { BOTPREFIX, TOKEN} = process.env;
 const commandList = {play, help, list}
 
 client.once("ready", () => console.log("ready"));
@@ -20,14 +19,11 @@ client.on("message", (message) => {
    const arg = message.content.slice(BOTPREFIX.length).trim().split(" ");
    const command = arg.shift().toLowerCase();
 
-   message.channel.send(`message received using the bot prefix ${BOTPREFIX}`);
-   message.channel.send(`responded message in ${message.guild.name}`);
    message.channel.send(`${message.author.username} has triggered a command`);
-   message.channel.send(`The command used for this message was ${command}`);
 
    try {
       //using computed property to call command sent in chatroom
-      commandList[command](message)
+      commandList[command](message, arg[0])
    } catch (error) {
       return message.channel.send(`you need to use a command from the list ${error}`)
    }
