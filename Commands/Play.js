034782d6f9c15dedcc319b73d06ac9e0e7875e7e
Gamerman2001 <Flash@heysparkbox.com>
2021-04-22 15:39:50 -0400

    feat: First edition of play (Deprecated and renewed in further commit) - our mp3Path variable is our local file location for our sound - our streampath var is a link to an mp3 - Right now play function will: 1.check if a member s in a voice channel 2.they they are then the bot will join the channel 3.the bot will play the file handed to it and console.log that it is finished playing or say you need to be in a voice channel

--- Commands/Play.js
+++ Commands/Play.js
@@ -1,7 +1,21 @@
 //this file will house all commands attached to the play command
+// import catSound from '../assets/CatSound.mp3'
+const mp3Path = path.resolve(
+   "/Users/flashgooden/Desktop/Sparkbox Development/apprentice-repo/discord-audio-bot/assets/CatSound.mp3"
+);
+const streamPath = "http://www.sample-videos.com/audio/mp3/wave.mp3";
 
-const play = async ()=> {
-  console.log('Play Command was called')
-}
+const play = async (message) => {
+   // Only try to join the sender's voice channel if they are in one themselves
+   if (message.member.voice.channel) {
+      const connection = await message.member.voice.channel.join();
+      const dispatcher = connection.play(streamPath, { volume: 1 });
+      dispatcher.on("finish", () => {
+         console.log("Finished playing!");
+      });
+   } else {
+      message.reply("You need to join a voice channel first!");
+   }
+};
 
-export default play 
+export default play;
