09e8e7439e0b1978034c0ebe56a6bba494d9891f
Gamerman2001 <Flash@heysparkbox.com>
2021-04-22 16:42:41 -0400

    Refactor & Feat I was looking to get a feature working so I imported the files needed for firebase into this commit. This made me have to alter my playMp3 function to incorporate the firebase api - (line 1 -2 ) we import the firebase api - we import xhr2 needed for our firebase link requests - firebaseConfig is all our info needed to identify the storage cluster - firebaseApp initializes firebase - (line 21-24) are references to all our firebase sounds - (26-33) is how we transform our references into URLS from firebase - refactor playMp3 : 1. using the new arg parameter passed in we compare it against the sound references then plays with the corresponding URL or return null

--- Commands/Play.js
+++ Commands/Play.js
@@ -1,23 +1,75 @@
 //this file will house all commands attached to the play command
-// import catSound from '../assets/CatSound.mp3'
-const mp3Path = path.resolve(
-   "/Users/flashgooden/Desktop/Sparkbox Development/apprentice-repo/discord-audio-bot/assets/CatSound.mp3"
-);
-const streamPath = "http://www.sample-videos.com/audio/mp3/wave.mp3";
-const playMp3 = (roomConnection) => {
-   roomConnection
-      .play(mp3Path, { volume: 1 })
-      .on("finish", () => {
-         console.log("Finished playing!");
-      });
+import firebase from "@firebase/app";
+import "@firebase/storage";
+//xhr2 is needed for asynchronous reference calls
+import xhr2 from "xhr2";
+global.XMLHttpRequest = xhr2;
+
+const firebaseConfig = {
+   apiKey: process.env.FIREBASEAPIKEY,
+   authDomain: process.env.FIREBASEAUTHDOMAIN,
+   projectId: process.env.FIREBASEPROJECTID,
+   storageBucket: "soundwave-bot-14c81.appspot.com",
+   messagingSenderId: process.env.FIREBASEMESSAGINGSENDERID,
+   appId: process.env.FIREBASEAPPID,
+   measurementId: process.env.FIREBASEMEASUREMENTID,
 };
 
-const userInVoiceChannel = (message)=> message.member.voice.channel ? true : false
-const roomConnection = (message) => message.member.voice.channel.join()
+const firebaseApp = firebase.default.initializeApp(firebaseConfig);
+const storage = firebaseApp.storage();
+//references to location of files in storage *Case-Sensitive*
+const catStorageRef = storage.ref("Cat-Sound.mp3");
+const battleStorageRef = storage.ref("soundscrate-battle-narrator-replay.mp3");
+const fightStorageRef = storage.ref("soundscrate-battle-narrator-round-1-fight.mp3");
+const jazzStorageRef = storage.ref("Jazz.m4a");
 
-const play = async (message) => {
+const firebaseCat = async () =>
+   await catStorageRef.getDownloadURL().then((url) => url);
+const firebaseBattle = async () =>
+   await battleStorageRef.getDownloadURL().then((url) => url);
+const fireBaseFight = async () =>
+   await fightStorageRef.getDownloadURL().then((url) => url);
+const fireBaseJazz = async () =>
+   await jazzStorageRef.getDownloadURL().then((url) => url);
+
+// const mp3Path = path.resolve( "/Users/flashgooden/Desktop/Sparkbox Development/apprentice-repo/discord-audio-bot/assets/CatSound.mp3");
+
+const playMp3 = async (roomConnection, arg) => {
+   arg === "cat"
+      ? roomConnection
+           .play(await firebaseCat(), { volume: 1 })
+           .on("finish", () => {
+              console.log("Finished playing!");
+           })
+      : arg === "replay"
+      ? roomConnection
+           .play(await firebaseBattle(), { volume: 1 })
+           .on("finish", () => {
+              console.log("Finished playing!");
+           })
+      : arg === "fight"
+      ? roomConnection
+           .play(await fireBaseFight(), { volume: 1 })
+           .on("finish", () => {
+              console.log("Finished playing!");
+           })
+      : arg === "jazz"
+      ? roomConnection
+           .play(await fireBaseJazz(), { volume: 1 })
+           .on("finish", () => {
+              console.log("Finished playing!");
+           })
+      : null;
+};
+
+const userInVoiceChannel = (message) =>
+   message.member.voice.channel ? true : false;
+const roomConnection = (message) => 
+   message.member.voice.channel.join();
+
+const play = async (message, arg) => {
    if (userInVoiceChannel(message)) {
-      playMp3(await roomConnection(message))
+      playMp3(await roomConnection(message), arg);
    } else {
       message.reply("You need to join a voice channel first!");
    }
