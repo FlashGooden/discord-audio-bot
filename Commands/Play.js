72aa3172b61028d644003afd3ed16178ad1f803a
Gamerman2001 <Flash@heysparkbox.com>
2021-04-22 15:48:57 -0400

    refactor: abstract play function -abstracted play function by having a playMp3 function -playMp3 will take in a room connection object provided by discord - the roomConnection object passed in will play an mp3 URL that it is passed - I created an abstraction function for checking if a mbmer is in a voice channel - I created an abstraction croomConnection that lets the bot join the voice channel -(line 18) play function now uses those abstracted functions

--- Commands/Play.js
+++ Commands/Play.js
@@ -4,15 +4,20 @@ const mp3Path = path.resolve(
    "/Users/flashgooden/Desktop/Sparkbox Development/apprentice-repo/discord-audio-bot/assets/CatSound.mp3"
 );
 const streamPath = "http://www.sample-videos.com/audio/mp3/wave.mp3";
-
-const play = async (message) => {
-   // Only try to join the sender's voice channel if they are in one themselves
-   if (message.member.voice.channel) {
-      const connection = await message.member.voice.channel.join();
-      const dispatcher = connection.play(streamPath, { volume: 1 });
-      dispatcher.on("finish", () => {
+const playMp3 = (roomConnection) => {
+   roomConnection
+      .play(mp3Path, { volume: 1 })
+      .on("finish", () => {
          console.log("Finished playing!");
       });
+};
+
+const userInVoiceChannel = (message)=> message.member.voice.channel ? true : false
+const roomConnection = (message) => message.member.voice.channel.join()
+
+const play = async (message) => {
+   if (userInVoiceChannel(message)) {
+      playMp3(await roomConnection(message))
    } else {
       message.reply("You need to join a voice channel first!");
    }
