f13401fd0a53fc53cbe1e7fa6d990e4cee240497
Gamerman2001 <Flash@heysparkbox.com>
2021-04-30 00:02:19 -0400

    test: updated play test - updated description of the play function - removed unecessary async await on test

--- Commands/Play.spec.js
+++ Commands/Play.spec.js
@@ -1,7 +1,7 @@
-import play from "./Play";
+import { play } from "./Play";
 
-describe("Tests the play function", () => {
-   it("will error if you are not in a voice channel", async () => {
+describe("The Play function", () => {
+   it("throws an error if you are not in a voice channel", () => {
       const mockArg = jest.fn();
       const mockMessage = jest.fn(() => {
          return {
@@ -10,11 +10,13 @@ describe("Tests the play function", () => {
          };
       });
       mockArg.mockReturnValue("cat");
-      await play(mockMessage('You need to join a voice channel first!'), mockArg);
-      expect(mockMessage).toHaveBeenCalledWith("You need to join a voice channel first!")
+      play(mockMessage("You need to join a voice channel first!"), mockArg);
+      expect(mockMessage).toHaveBeenCalledWith(
+         "You need to join a voice channel first!"
+      );
    });
 
-   it("will join channel to play mp3 if command is available and user is in room", async () => {
+   it("joins a channel to play mp3 if command is available and user is in room", () => {
       let joinedChannel = 0;
       const mockMessage = jest.fn();
       mockMessage.mockReturnValue({
@@ -24,7 +26,7 @@ describe("Tests the play function", () => {
       const mockArg = jest.fn();
       mockArg.mockReturnValue("cat");
 
-      await play(mockMessage(), mockArg);
+      play(mockMessage(), mockArg);
       expect(joinedChannel).toEqual(1);
    });
 });
