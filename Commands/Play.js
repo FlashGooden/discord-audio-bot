1717d6d37a765603ecb74d49d766ecd0519ef842
Gamerman2001 <Flash@heysparkbox.com>
2021-04-29 23:19:57 -0400

    style: styling adjustments

--- Commands/Play.js
+++ Commands/Play.js
@@ -14,14 +14,17 @@ const firebaseConfig = {
    appId: process.env.FIREBASEAPPID,
    measurementId: process.env.FIREBASEMEASUREMENTID,
 };
-
+//firebase initialization
 const firebaseApp = firebase.default.initializeApp(firebaseConfig);
 const storage = firebaseApp.storage();
 //references to location of files in storage *Case-Sensitive*
 const catStorageRef = storage.ref("Cat-Sound.mp3");
 const battleStorageRef = storage.ref("soundscrate-battle-narrator-replay.mp3");
-const fightStorageRef = storage.ref("soundscrate-battle-narrator-round-1-fight.mp3");
+const fightStorageRef = storage.ref(
+   "soundscrate-battle-narrator-round-1-fight.mp3"
+);
 const jazzStorageRef = storage.ref("Jazz.m4a");
+const helicopterStorageRef = storage.ref("soundscrate-helicopterloop2.mp3");
 
 const firebaseCat = async () =>
    await catStorageRef.getDownloadURL().then((url) => url);
@@ -31,58 +34,42 @@ const fireBaseFight = async () =>
    await fightStorageRef.getDownloadURL().then((url) => url);
 const fireBaseJazz = async () =>
    await jazzStorageRef.getDownloadURL().then((url) => url);
+const fireBaseHelicopter = async () =>
+   await helicopterStorageRef.getDownloadURL().then((url) => url);
 
 // const mp3Path = path.resolve( "/Users/flashgooden/Desktop/Sparkbox Development/apprentice-repo/discord-audio-bot/assets/CatSound.mp3");
+export const playInDiscord = async (roomConnection, url) => {
+   roomConnection.play(await url(), { volume: 1 }).on("finish", () => {
+      console.log("Finished playing!");
+   });
+};
 
 export const playMp3 = async (roomConnection, arg) => {
    switch (arg) {
       case "cat":
-         roomConnection
-            .play(await firebaseCat(), { volume: 1 })
-            .on("finish", () => {
-               console.log("Finished playing!");
-            });
+         playInDiscord(roomConnection, firebaseCat);
          break;
       case "replay":
-         roomConnection
-            .play(await firebaseBattle(), { volume: 1 })
-            .on("finish", () => {
-               console.log("Finished playing!");
-            });
+         playInDiscord(roomConnection, firebaseBattle);
          break;
       case "jazz":
-         roomConnection
-            .play(await fireBaseJazz(), { volume: 1 })
-            .on("finish", () => {
-               console.log("Finished playing!");
-            });
+         playInDiscord(roomConnection, fireBaseJazz);
          break;
       case "helicopter":
-         roomConnection
-            .play(await fireBaseHelicopter(), { volume: 1 })
-            .on("finish", () => {
-               console.log("Finished playing!");
-            });
+         playInDiscord(roomConnection, fireBaseHelicopter);
          break;
       case "fight":
-         roomConnection
-            .play(await fireBaseFight(), { volume: 1 })
-            .on("finish", () => {
-               console.log("Finished playing!");
-            });
+         playInDiscord(roomConnection, fireBaseFight);
          break;
       default:
          null;
    }
-  
 };
 
-const userInVoiceChannel = (message) =>
-   message.member.voice.channel ? true : false;
-const roomConnection = (message) => 
-   message.member.voice.channel.join();
+const userInVoiceChannel = (message) => !!message.member.voice.channel;
+const roomConnection = (message) => message.member.voice.channel.join();
 
-const play = async (message, arg) => {
+export const play = async (message, arg) => {
    if (userInVoiceChannel(message)) {
       playMp3(await roomConnection(message), arg);
    } else {
