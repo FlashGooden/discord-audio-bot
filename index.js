adb3ced94f2d7603a9c6e52a92410a86a3772f4e
Gamerman2001 <Flash@heysparkbox.com>
2021-05-01 16:18:11 -0400

    style: styling fixed

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import dotenv from "dotenv";
 import Discord from "discord.js";
-import play from './Commands/Play.js'
-import help from './Commands/Help.js'
-import list from './Commands/List.js'
+import play from "./Commands/Play.js";
+import help from "./Commands/Help.js";
+import list from "./Commands/List.js";
 
-//setup and config 
+//setup and config
 const client = new Discord.Client();
 dotenv.config();
 
-const { BOTPREFIX, TOKEN} = process.env;
-const commandList = {play, help, list}
+const { BOTPREFIX, TOKEN } = process.env;
+const commandList = { play, help, list };
 
 client.once("ready", () => console.log("ready"));
 
@@ -23,9 +23,11 @@ client.on("message", (message) => {
 
    try {
       //using computed property to call command sent in chatroom
-      commandList[command](message, arg[0])
+      commandList[command](message, arg[0]);
    } catch (error) {
-      return message.channel.send(`you need to use a command from the list ${error}`)
+      return message.channel.send(
+         `you need to use a command from the list ${error}`
+      );
    }
 });
 
